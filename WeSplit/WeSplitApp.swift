102241720d323da6a69ea5cceee8b56b97e59c23
Om Preetham <ompreethambandi@gmail.com>
2024-05-14 14:20:27 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Om Preetham Bandi on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
